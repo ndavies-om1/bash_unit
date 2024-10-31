5917327983f5444d9730639fa06bc29e53a431ec
ndavies-om1 <ndavies@om1.com>
2024-10-31 10:15:00 -0400

    forcing to publish to mine

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ CURL="curl --show-error --silent --location"
 
 echo "downloading bash_unit"
 current_working_dir=$PWD
-tarball_urls=$($CURL https://api.github.com/repos/pgrange/bash_unit/releases | grep tarball_url)
+tarball_urls=$($CURL https://api.github.com/repos/ndavies-om1/bash_unit/releases | grep tarball_url)
 tarball_url=$(echo "$tarball_urls" | head -n 1 | cut -d '"' -f 4)
 tmp_dir=$(mktemp -d 2>/dev/null || mktemp -d -t 'tmpdir')
 cd "$tmp_dir" || exit
